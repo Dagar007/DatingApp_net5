01ab22cc84aa9e41d68bea2fe19e0c77b2c6b5bf
Deepak Kumar <deepak.kumar11@nagarro.com>
2020-12-01 19:24:27 +0530

    End of Section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -23,6 +23,7 @@ namespace API
             {
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -43,6 +44,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
